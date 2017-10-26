641fc8436ad79b5ae48c3bfd45cdccd0a533234a
Gary Gu <gu.gary@gmail.com>
2017-10-26 12:26:35 -0400

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    alert(profile.getEmail());
+};
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+	console.log('User signed out.');
+    });
+}
